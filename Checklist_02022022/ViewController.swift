54b0fafe63cb7a703639f0538a3b23abdc9b44c9
furkanSengil <70530027+furkanSengil@users.noreply.github.com>
2022-02-02 13:04:20 +0300

    Initial Commit

---
+++ Checklist_02022022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklist_02022022
+//
+//  Created by David Thompson on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
